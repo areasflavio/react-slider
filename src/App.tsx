200ba65dabd1ebac463d947c25e1be407139729e
Flávio Arêas <vflavio9@gmail.com>
2022-06-11 19:55:40 -0300

    feat: create framer motion animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import { motion } from 'framer-motion';
+import { useEffect, useRef, useState } from 'react';
+import './App.css';
+import pictures from './assets/pictures/index';
+
 function App() {
+  const carouselRef = useRef<HTMLDivElement>(null);
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    setWidth(
+      (carouselRef.current?.scrollWidth ?? 0) -
+        (carouselRef.current?.offsetWidth ?? 0)
+    );
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <motion.div
+        className="carousel"
+        ref={carouselRef}
+        whileTap={{ cursor: 'grabbing' }}
+      >
+        <motion.div
+          className="inner-carousel"
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+        >
+          {pictures.map((pic) => (
+            <motion.div key={pic} className="item">
+              <img src={pic} alt="" />
+            </motion.div>
+          ))}
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
---
+++ src/assets/pictures/index.ts
@@ -0,0 +1,8 @@
+import picture1 from './picture1.jpg';
+import picture2 from './picture2.jpg';
+import picture3 from './picture3.jpg';
+import picture4 from './picture4.jpg';
+import picture5 from './picture5.jpg';
+import picture6 from './picture6.jpg';
+
+export default [picture1, picture2, picture3, picture4, picture5, picture6];
